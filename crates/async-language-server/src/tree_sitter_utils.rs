39e9b99583ea921b3a3022712010af8bc0856e56
Filip Tibell <filip.tibell@gmail.com>
2025-05-12 22:01:29 +0200

    Add some comments to tree sitter utils in lsp crate

--- crates/async-language-server/src/tree_sitter_utils.rs
+++ crates/async-language-server/src/tree_sitter_utils.rs
@@ -1,6 +1,10 @@
 use async_lsp::lsp_types::{Position as LspPosition, Range as LspRange};
 use tree_sitter::{Point as TsPoint, Range as TsRange};
 
+/**
+    Converts a tree sitter `Point` to an LSP `Position`
+*/
+#[must_use]
 pub const fn ts_point_to_lsp_position(pos: TsPoint) -> LspPosition {
     LspPosition {
         line: pos.row as u32,
@@ -8,6 +12,10 @@ pub const fn ts_point_to_lsp_position(pos: TsPoint) -> LspPosition {
     }
 }
 
+/**
+    Converts a tree sitter `Range` to an LSP `Range`
+*/
+#[must_use]
 pub const fn ts_range_to_lsp_range(range: TsRange) -> LspRange {
     LspRange {
         start: ts_point_to_lsp_position(range.start_point),
@@ -15,11 +23,26 @@ pub const fn ts_range_to_lsp_range(range: TsRange) -> LspRange {
     }
 }
 
-pub fn ts_range_contains_lsp_position(range: TsRange, pos: LspPosition) -> bool {
+/**
+    Returns `true` if the given tree sitter `Range`
+    contains the given LSP `Position`, otherwise `false`
+
+    This is an **inclusive** bounds check, meaning the position is
+    considered *inside* even if it lies on a line or column boundary
+*/
+#[must_use]
+pub const fn ts_range_contains_lsp_position(range: TsRange, pos: LspPosition) -> bool {
     let point = lsp_position_to_ts_point(pos);
-    range.start_point <= point && point <= range.end_point
+    point.row >= range.start_point.row
+        && point.column >= range.start_point.column
+        && point.row <= range.end_point.row
+        && point.column <= range.end_point.column
 }
 
+/**
+    Converts an LSP `Position` to a tree sitter `Point`
+*/
+#[must_use]
 pub const fn lsp_position_to_ts_point(pos: LspPosition) -> TsPoint {
     TsPoint {
         row: pos.line as usize,
