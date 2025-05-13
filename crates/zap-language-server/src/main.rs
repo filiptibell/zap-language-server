855892894ad626952bff98f7c049076ff6d2c4ba
Filip Tibell <filip.tibell@gmail.com>
2025-05-14 00:13:08 +0200

    Enable only the necessary tokio features

--- crates/zap-language-server/src/main.rs
+++ crates/zap-language-server/src/main.rs
@@ -8,7 +8,7 @@ mod utils;
 
 use self::tracing::setup_tracing;
 
-#[tokio::main]
+#[tokio::main(flavor = "current_thread")]
 async fn main() -> anyhow::Result<()> {
     setup_tracing();
     cli::Cli::new().run().await
