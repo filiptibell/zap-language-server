57e2244b05f24045255f8c8b74f2119836968fa4
Filip Tibell <filip.tibell@gmail.com>
2025-05-13 18:28:06 +0200

    Add tiny bash script to make a file with all roblox class names

---
+++ scripts/fetch_roblox_classes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+URL="https://raw.githubusercontent.com/MaximumADHD/Roblox-Client-Tracker/refs/heads/roblox/API-Dump.json"
+OUTPUT_FILE="crates/zap-language-server/src/docs/roblox-class-names.txt"
+
+if ! command -v jq &> /dev/null; then
+    echo "jq is not installed"
+    exit 1
+fi
+
+curl -s "$URL" | jq -r '.Classes[].Name' | sort | uniq > "$OUTPUT_FILE"
+
+echo "Extracted unique class names to $OUTPUT_FILE"
