3472aeac41611224d90de9c6fc1d68ea8c322dd0
Filip Tibell <filip.tibell@gmail.com>
2025-05-14 17:31:48 +0200

    Remove old tracing env filter rules

--- crates/zap-language-server/src/tracing.rs
+++ crates/zap-language-server/src/tracing.rs
@@ -15,13 +15,8 @@ pub fn setup_tracing() {
             LevelFilter::INFO.into()
         })
         .from_env_lossy()
-        .add_directive("rustls=warn".parse().unwrap())
         .add_directive("tower_lsp=warn".parse().unwrap())
-        .add_directive("tower=info".parse().unwrap())
-        .add_directive("h2=info".parse().unwrap())
-        .add_directive("hyper=info".parse().unwrap())
-        .add_directive("rustls=info".parse().unwrap())
-        .add_directive("reqwest=info".parse().unwrap());
+        .add_directive("tower=info".parse().unwrap());
 
     tracing_subscriber::fmt()
         .compact()
