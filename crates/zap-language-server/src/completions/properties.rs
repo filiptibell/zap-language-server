45ee3d23c7df8388ac5a07864b689b2ae477c953
Filip Tibell <filip.tibell@gmail.com>
2025-05-14 17:27:09 +0200

    Update to latest grammar and fix property completions

--- crates/zap-language-server/src/completions/properties.rs
+++ crates/zap-language-server/src/completions/properties.rs
@@ -2,29 +2,56 @@ use async_language_server::{
     lsp_types::{CompletionItemKind, Position},
     server::Document,
     tree_sitter::Node,
+    tree_sitter_utils::{find_child, ts_range_contains_lsp_position},
 };
 use zap_language::docs::find_variants;
 
-pub fn completion(doc: &Document, _pos: Position, node: Node) -> Vec<(CompletionItemKind, String)> {
-    let Some(parent) = node.parent() else {
+pub fn completion(_doc: &Document, pos: Position, node: Node) -> Vec<(CompletionItemKind, String)> {
+    // If our current node is the top-level "source file" we can
+    // probably drill down to something a bit more specific & useful
+    let node = if node.kind() == "source_file" {
+        find_child(node, |c| {
+            ts_range_contains_lsp_position(c.range(), pos)
+                && matches!(c.kind(), "event_declaration" | "function_declaration")
+        })
+        .unwrap_or(node)
+    } else {
+        node
+    };
+
+    // Try to find the field node
+    let node = find_child(node, |d| {
+        ts_range_contains_lsp_position(d.range(), pos) && is_property_field(d)
+    })
+    .unwrap_or(node);
+
+    // Try to find the value node
+    let node = node.child_by_field_name("value").unwrap_or(node);
+
+    // We should now be inside the value node, and have field as parent
+    let Some(parent) = node.parent().filter(|n| is_property_field(*n)) else {
         return Vec::new();
     };
 
     let mut items = Vec::new();
 
-    let is_event = parent.kind().starts_with("event_");
-    let is_funct = parent.kind().starts_with("function_");
-    let is_field = parent.kind().ends_with("_field");
-    if (is_event || is_funct) && is_field {
-        let field_name = doc.text().byte_slice(parent.byte_range());
-        if let Some((false, variants)) = find_variants([field_name]) {
-            items.extend(
-                variants
-                    .iter()
-                    .map(|word| (CompletionItemKind::ENUM_MEMBER, (*word).to_string())),
-            );
-        }
+    if let Some((false, variants)) = find_variants([parent.kind()]) {
+        items.extend(
+            variants
+                .iter()
+                .map(|word| (CompletionItemKind::ENUM_MEMBER, (*word).to_string())),
+        );
     }
 
     items
+}
+
+fn is_property_field(node: Node) -> bool {
+    let kind = node.kind();
+
+    let is_event = kind.starts_with("event_");
+    let is_funct = kind.starts_with("function_");
+    let is_field = kind.ends_with("_field");
+
+    (is_event || is_funct) && is_field
 }
