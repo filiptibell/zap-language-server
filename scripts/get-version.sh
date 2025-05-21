ab0bd857e08e850f9b4c9b6eba7274f55b3ce056
Filip Tibell <filip.tibell@gmail.com>
2025-05-21 21:32:17 +0200

    Add release workflow and editorconfig

---
+++ scripts/get-version.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Since we are using cargo workspaces, reading the actual version
+# of the CLI is slightly more complicated - which is why this exists
+
+CLI_MANIFEST=$(cargo read-manifest --manifest-path crates/zap-language-server/Cargo.toml)
+CLI_VERSION=$(echo $CLI_MANIFEST | jq -r .version)
+
+echo $CLI_VERSION
